1959996bd8c6c71db2d3345dd5783cdeead04005
sushil711 <107679104+sushil711@users.noreply.github.com>
2022-06-17 17:39:18 +0530

    Add files via upload

---
+++ Recursion.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fun(int n)
+{
+	if(n>0)
+	{
+	    
+		cout<<n<<" ";
+		fun(n-1);
+		fun(n-1);
+	
+		
+	}
+	
+}
+
+int main()
+{
+	int x=3;
+	fun(x);
+	
+	return 0;
+}
