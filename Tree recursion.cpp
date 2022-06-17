620033344174ca1b0091eda53ceb27606d3ff790
SushilSharma <sushil.sharma@pw.live>
2022-06-17 17:33:10 +0530

    my third commit

---
+++ Tree recursion.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int fun(int n)
+{
+	if(n>0)
+	{
+	    
+		
+		cout<<n<<" ";
+		fun(n-1);
+		
+		fun(n-1);
+		
+		return 0;
+	}
+	
+}
+
+int main()
+{
+	int x=3;
+	fun(x);
+	
+	return 0;
+}
